6712819f94c758b61c01406ef3171d3c228b3bc1
Dmitriy Manuylov <dmitriy.a.manuylov@yandex.ru>
2026-05-02 15:17:37 +0300

    Вынесение парсера в отдельный проект.

---
+++ PgQueryParser/CustomExceptions/ParseQueryException.cs
@@ -0,0 +1,19 @@
+﻿using PgQueryParser.LibPgQueryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PgQueryParseLib.CustomExceptions
+{
+    public class ParseQueryException: Exception
+    {
+        public ParseQueryException(ParseError parseError)
+        {
+            ParseError = parseError;
+        }
+
+        public ParseError ParseError { get; }
+    }
+}
---
+++ PgQueryParser/CustomExceptions/PlStmtParseException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PgQueryParseLib.CustomExceptions
+{
+    public class PlStmtParseException: Exception
+    {
+        public PlStmtParseException(
+            string message,
+            Exception? innerException = null): base(message, innerException)
+        { 
+
+        }
+    }
+}
